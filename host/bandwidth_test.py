27a22a521516a7b26238f7dbd982202edb44ea43
Grant Haack <granthaack@protonmail.com>
2019-02-02 23:11:20 -0800

    working on bandwidth tester

--- host/bandwidth_test.py
+++ host/bandwidth_test.py
@@ -1,5 +1,7 @@
 import socket
 from threading import Thread, Event
+import time
+import signal
 
 PORT = 3333
 SIZE = 1024
@@ -11,40 +13,77 @@ class ParentTsock:
     connections = []
     stop_event = Event()
 
-    def __init__(self):
-        self.parent_sock.bind((socket.gethostname(), PORT))
-        print("Socket started on address {} and port {}".format(self.parent_sock.getsockname(), PORT))
+    def __init__(self, addr):
+        self.parent_sock.bind((addr, PORT))
+        print("Socket started on address {} and port {}".format(self.parent_sock.getsockname()[0], PORT))
 
     def listen_sock(self):
+        """
+        Listen for socket connections from ESP32s. When a one connects, spawn a new thread and push data to it
+        :return:
+        """
         self.parent_sock.listen(0)
-        print("Listening for a connections...")
+        print("Listening for connections...")
         while not self.stop_event.is_set():
             (conn, addr) = self.parent_sock.accept()
             print("Got connection from {}".format(addr))
-            child_sock = SockThread(conn, addr, SIZE)
+            child_sock = SockThread(conn, addr)
             child_sock.run()
             self.connections.append(child_sock)
+
+        if self.connections:
+            for connection in self.connections:
+                connection.stop()
+
+    def stop(self):
+        """
+        Ask the thread and all children threads to stop
+        :return:
+        """
+        self.stop_event.set()
 
 
 class SockThread(Thread):
 
     sock = None
     addr = None
-    size = None
     stop_event = Event()
 
-    def __init__(self, conn, addr, size):
+    def __init__(self, conn, addr):
         self.sock = conn
         self.addr = addr
-        self.size = size
 
     def run(self):
         self.push_data()
 
     def push_data(self):
+        """
+        Push data to an ESP32 and measure the bandwidth
+        :return:
+        """
+        buff = []
+        for i in range(0, SIZE):
+            buff.append(b"A")
         while not self.stop_event.is_set():
-            pass
+            timestamp = time.time()
+            ret = self.sock.sendall(buff)
+            if ret is None:
+                print("Speed: {} bytes per second on connection to {}".format(SIZE/(time.time() - timestamp), self.addr))
+            else:
+                print("Connection error: sock.sendall() returned {} instead of None".format(ret))
+
+    def stop(self):
+        """
+        Stop the thread
+        :return:
+        """
+        self.stop_event.set()
+
 
 def main():
-    sock = ParentTsock()
+    sock = ParentTsock("192.168.1.18")
     sock.listen_sock()
+
+
+if __name__ == '__main__':
+    main()
